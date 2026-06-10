b314584870bcb5cc4254a28fdad920e246d9754b
UdyaRodrigo <udya.rodrigo24@imperial.ac.uk>
2026-06-10 13:30:51 +0100

    Update Battleships.test.js

--- web-app/tests/Battleships.test.js
+++ web-app/tests/Battleships.test.js
@@ -1,7 +1,7 @@
 // still needs further edits, and comments and more graphics, any input or improvements will be useful
 
 import assert from "node:assert/strict";
-import Battleships from ".../Battleships.js";
+import Battleships from "../Battleships.js";
  
 const fire_sequence = function (moves) {
   return moves.reduce(function (game, [row,column]) {
@@ -18,7 +18,7 @@ describe("New game", function () {
   it("player 1 starts, prepare to fire", function () {
     const game = Battleships.new_game();
 
-     assert.equal(Battlleships.current_player(game), 1);
+     assert.equal(Battleships.current_player(game), 1);
   });
 
   it("starts with no attacks from either player", function () {
@@ -29,15 +29,15 @@ describe("New game", function () {
   });
 });
 
-describe("Attacks" function () {
+describe("Attacks", function () {
   it("allows the current player to fire at an unattacked square", function () {
     const game = Battleships.new_game();
 
     assert.equal(Battleships.is_legal_attack(game, 1, 0, 0), true);
   });
   
-  it("changes turn after a legal attack") function () {
-    const game = Battleships.fire_at/Battleships.new_game(), 1, 0, 0);
+  it("changes turn after a legal attack", function () {
+    const game = Battleships.fire_at(Battleships.new_game(), 1, 0, 0);
   
     assert.equal(Battleships.current_player(game), 2);
   });
@@ -67,9 +67,9 @@ describe("Winning", function () {
       [4, 1],
       [0, 3],
       [5, 1]
-    ]):
+    ]);
 
-    asser.equal(Battleships.winner(game), 1);
+    assert.equal(Battleships.winner(game), 1);
     assert.equal(Battleships.is_ended(game), true);
   });
 });
