d2205a67a786ca9ba52721ca06aa7b86a7b77643
UdyaRodrigo <udya.rodrigo24@imperial.ac.uk>
2026-07-05 18:51:19 +0100

    Update server.mjs

--- web-app/server.mjs
+++ web-app/server.mjs
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 8080;
 
+app.use("/node_modules", express.static("node_modules"));
 app.use(express.static("web-app"));
 
 app.listen(port, function () {
