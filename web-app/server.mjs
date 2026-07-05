41dd7232f6c2f20f3bc031d19034433c0601bb5a
UdyaRodrigo <udya.rodrigo24@imperial.ac.uk>
2026-07-05 16:53:32 +0100

    Update server.mjs

--- web-app/server.mjs
+++ web-app/server.mjs
@@ -1,1 +1,10 @@
+import express from "express";
 
+const app = express();
+const port = 8080;
+
+app.use(express.static("web-app"));
+
+app.listen(port, function () {
+    console.log(`Battleships is running at http://localhost:${port}`);
+});
